d93e4dca5cc7f2902d53fe9c20be144f8fa282e4
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-03-10 22:28:07 +0530

    Pulling up stuff

---
+++ Assets/Scripts/SampleCompute.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleCompute : MonoBehaviour
+{
+    public ComputeShader cs;
+    public RenderTexture result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int kernel = cs.FindKernel("CSMain");
+
+        result = new RenderTexture(1024, 512, 24);
+        result.enableRandomWrite = true;
+        result.Create();
+        cs.SetTexture(kernel, "Result", result);
+        cs.Dispatch(kernel, 512/128, 512/1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
