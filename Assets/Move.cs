46a618eb1db06145f72cb90c6259e35f46fbe8b4
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-04-01 22:34:46 +0530

    Something is building up

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform cubePosition;
+    public ComputeShader moveComputeShader;
+    public float offset = 1f;
 
-    // Update is called once per frame
-    void Update()
+    private ComputeBuffer moveBuffer;
+    private int idToKernel;
+
+    private const int STRIDE = 3 * sizeof(float);
+
+    private void OnEnable()
     {
-        
+        moveBuffer = new ComputeBuffer(1, STRIDE, ComputeBufferType.Structured, ComputeBufferMode.Dynamic);
+        idToKernel = moveComputeShader.FindKernel("CSMain");
+        moveComputeShader.SetBuffer(idToKernel, "Result", moveBuffer);
     }
 }
