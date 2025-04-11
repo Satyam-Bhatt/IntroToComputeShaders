bce9cd2882a112157823287a40926766be3e1d5e
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-04-11 22:48:29 +0530

    Lets add lights and make it animate

--- Assets/Second/MillionCubes.cs
+++ Assets/Second/MillionCubes.cs
@@ -6,6 +6,7 @@ public class MillionCubes : MonoBehaviour
 {
     [SerializeField] private Mesh instanceMesh;
     [SerializeField] private Material instanceMaterial;
+    [SerializeField] private int count = 0;
 
     private ComputeBuffer argsBuffer;
     private const int ARGS_STRIDE = sizeof(uint) * 5;
@@ -18,7 +19,7 @@ public class MillionCubes : MonoBehaviour
         argsBuffer = new ComputeBuffer(1, ARGS_STRIDE, ComputeBufferType.IndirectArguments);
         argsBuffer.SetData(new int[] { 0, 1, 0, 0, 0 });
 
-        positionBuffer = new ComputeBuffer(2, POSITION_STRIDE);
+        positionBuffer = new ComputeBuffer(count, POSITION_STRIDE);
         //positionBuffer.SetData(new float[] { 0, 0, 0, 0 });
     }
 
@@ -48,16 +49,29 @@ public class MillionCubes : MonoBehaviour
     {
         uint[] _args = { 0, 1, 0, 0, 0 };
         _args[0] = instanceMesh.GetIndexCount(0);
-        _args[1] = 2;
+        _args[1] = (uint)count;
         _args[2] = instanceMesh.GetIndexStart(0);
         _args[3] = instanceMesh.GetBaseVertex(0);
         _args[4] = 0; // Instance Start
 
         argsBuffer.SetData(_args);
 
-        Vector4[] _positions = { 
-            new Vector4(0, 0, 0, 0),
-            new Vector4(2, 2, 2, 0) };
+        Vector4[] _positions = new Vector4[count];
+
+        for (int i = 0; i < _positions.Length; i++)
+        {
+            if(i == 0)
+            {
+                _positions[i] = new Vector4(0, 0, 0, 0);
+            }
+            else
+            {
+                _positions[i] = new Vector4(i, 0, 0, 0);
+            }
+            //for (int j = 0; j < 100; j++)
+            //{
+            //}
+        }
 
         positionBuffer.SetData(_positions);
 
