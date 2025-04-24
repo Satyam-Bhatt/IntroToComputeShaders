4c1a8e529a6df57ca102ddab3e5bd544e30d7bfa
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-04-24 22:29:59 +0530

    Material also done

--- Assets/InteractableSpheres/InteractableSpheres.cs
+++ Assets/InteractableSpheres/InteractableSpheres.cs
@@ -39,10 +39,61 @@ public class InteractableSpheres : MonoBehaviour
         positionBuffer = null;
     }
 
+    private void Start()
+    {
+        UpdateBuffer();
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         mover.Translate(dir * Time.deltaTime * 20);
+
+        computeShader.SetVector("position", mover.position);
+        computeShader.Dispatch(kernel, Mathf.CeilToInt(count / 64.0f), 1, 1);
+
+        Graphics.DrawMeshInstancedIndirect(instanceMesh, 0, instanceMaterial, new Bounds(Vector3.zero, Vector3.one * 1000), argsBuffer);
+    }
+
+    void UpdateBuffer()
+    {
+        uint[] _args = { 0, 1, 0, 0, 0 };
+        _args[0] = instanceMesh.GetIndexCount(0);
+        _args[1] = (uint)count;
+        _args[2] = instanceMesh.GetIndexStart(0);
+        _args[3] = instanceMesh.GetBaseVertex(0);
+        _args[4] = 0; // Instance Start
+
+        argsBuffer.SetData(_args);
+
+        Vector4[] _positions = new Vector4[count];
+
+        // Calculate dimensions for a cube grid
+        int dimension = Mathf.CeilToInt(Mathf.Pow(count, 1.0f / 3.0f));
+        float spacing = 1.5f; // Distance between cube centers
+
+        // Calculate the starting offset to center the grid
+        float offset = -spacing * (dimension - 1) / 2.0f;
+
+        for (int i = 0; i < count; i++)
+        {
+            // Convert 1D index to 3D coordinates
+            int x = i % dimension;
+            int y = (i / dimension) % dimension;
+            int z = i / (dimension * dimension);
+
+            // Position with equal spacing and centered around origin
+            float posX = offset + x * spacing;
+            float posY = offset + y * spacing;
+            float posZ = offset + z * spacing;
+
+            _positions[i] = new Vector4(posX, posY, posZ, 0);
+        }
+
+        positionBuffer.SetData(_positions);
+        computeShader.SetBuffer(kernel, "Result", positionBuffer);
+
+        instanceMaterial.SetBuffer("position", positionBuffer);
     }
 }
---
+++ Assets/InteractableSpheres/materialForSomething.shader
@@ -0,0 +1,57 @@
+Shader "Unlit/materialForSomething"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            StructuredBuffer<float4> position;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v, const uint id : SV_InstanceID) // What Difference between instance_id and vertexID
+            {
+                float4 startPos = position[id];
+                float3 world_pos  = startPos.xyz + v.vertex.xyz;
+
+                v2f o;
+                o.vertex = UnityObjectToClipPos(float4(world_pos, 1.0));
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
