12b50baade782466c7c119acd08b244e809552f3
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-06-01 18:53:17 +0530

    Finally a direction

--- Assets/Grass/Grass.cs
+++ Assets/Grass/Grass.cs
@@ -9,4 +9,8 @@ public class Grass : MonoBehaviour
     [SerializeField] private int instanceCount = 10;
     //Can use this to calculate the size and rotation of the mesh
     [SerializeField] private ComputeShader computeShader;
+    private int kernel;
+    private ComputeBuffer argsBuffer;
+    private const int ARGS_STRIDE = sizeof(uint) * 5;
+    private ComputeBuffer positionBuffer;
 }
