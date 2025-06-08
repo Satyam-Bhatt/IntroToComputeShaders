b5ee86fe276204fc619f5314e750694e4fd15ea4
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-06-08 16:24:27 +0530

    Grass is spawning yayyy

--- Assets/Grass/Grass.cs
+++ Assets/Grass/Grass.cs
@@ -6,7 +6,7 @@ public class Grass : MonoBehaviour
 {
     [SerializeField] private Mesh instanceMesh;
     [SerializeField] private Material instanceMaterial;
-    [SerializeField] private int instanceCount = 10;
+    [SerializeField] private int instanceCount = 16;
     //Can use this to calculate the size and rotation of the mesh
     [SerializeField] private ComputeShader computeShader;
     private int kernel;
@@ -21,5 +21,51 @@ public class Grass : MonoBehaviour
         argsBuffer.SetData(new uint[] { 0, 1, 0, 0, 0 });
         positionBuffer = new ComputeBuffer(instanceCount, POSITION_STRIDE);
         kernel = computeShader.FindKernel("CSMain");
+    }
+
+    private void OnDisable()
+    {
+        argsBuffer.Release();
+        argsBuffer = null;
+
+        positionBuffer.Release();
+        positionBuffer = null;
+    }
+
+    private void Start()
+    {
+        UpdateBuffer();
+    }
+
+    private void Update()
+    {
+        Graphics.DrawMeshInstancedIndirect(instanceMesh, 0, instanceMaterial, new Bounds(Vector3.zero, Vector3.one * 1000), argsBuffer);
+    }
+
+    private void UpdateBuffer()
+    {
+        uint[] _args = {0 , 1, 0, 0, 0 };
+        _args[0] = (uint)instanceMesh.GetIndexCount(0);
+        _args[1] = (uint)instanceCount;
+        _args[2] = instanceMesh.GetIndexStart(0);
+        _args[3] = instanceMesh.GetBaseVertex(0);
+        _args[4] = 0;
+
+        argsBuffer.SetData(_args);
+
+
+        Vector4[] positions = new Vector4[instanceCount];
+        for (int i = 0; i < instanceCount; i++)
+        {
+            positions[i] = new Vector4(Random.Range(-1f, 1f) + i, 0, Random.Range(-1f, 1f) + i, 1);
+        }
+        positionBuffer.SetData(positions);
+
+        computeShader.SetBuffer(kernel, "Result", positionBuffer);
+        instanceMaterial.SetBuffer("position", positionBuffer);
+
+        // Dispatch in Update shaders if we are updating the buffer every frame
+        computeShader.Dispatch(kernel, Mathf.CeilToInt(instanceCount / 8.0f), 1, 1);
+
     }
 }
---
+++ Assets/Grass/ShaderForGrass.shader
@@ -0,0 +1,67 @@
+Shader "Unlit/ShaderForGrass"
+{
+    Properties
+    {
+
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        Pass
+        {
+            Cull Off
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #pragma multi_compile_instancing // Enables GPU instancing
+
+            // Signal this shader requires compute buffers
+            #pragma prefer_hlslcc gles
+            #pragma exclude_renderers d3d11_9x
+            #pragma target 5.0
+
+            #include "UnityCG.cginc"
+            //#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            //#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+            StructuredBuffer<float4> position;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v, const uint id : SV_InstanceID)
+            {
+                float4 startPos = position[id];
+                float3 world_Pos = startPos.xyz + v.vertex.xyz;
+
+                v2f o;
+                //o.vertex = UnityObjectToClipPos(v.vertex);
+                o.vertex = UnityObjectToClipPos(world_Pos);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            float4 frag (v2f i) : SV_Target
+            {
+                float4 col = float4(1.0f, 0.4f, 1.0f, 1.0f);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
